cc4a9d1d77caf9ec8864e690f58b354ca88d4004
Sara Golemon <pollita@php.net>
2015-04-23 01:30:00 +0000

    [HHVM] Add a SystemLib

--- hhvm/config.cmake
+++ hhvm/config.cmake
@@ -1,2 +1,2 @@
 HHVM_EXTENSION(h3 ext_h3.cpp)
-
+HHVM_SYSTEMLIB(h3 ext_h3.php)
--- hhvm/ext_h3.cpp
+++ hhvm/ext_h3.cpp
@@ -6,6 +6,10 @@ class H3Extension : public Extension {
  public:
   H3Extension(): Extension("h3", "0.0.1") {}
 
+  void moduleInit() override {
+    loadSystemlib();
+  }
+
 } s_h3_extension;
 
 HHVM_GET_MODULE(h3);
